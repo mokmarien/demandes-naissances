95836bd580a5c9d594d2ea4f56e2e9cfb52118d2
Marien MOKOUMBOU <mokmarien@gmail.com>
2024-10-13 23:17:38 +0000

    Traitement d'une operation en selectionnant l'operation

--- script/calculatrice.js
+++ script/calculatrice.js
@@ -21,3 +21,22 @@ function gestionDuFormulaire(event) {
     const operationLabel = operation === "1" ? "+" : "*";
     document.getElementById("resultat").innerHTML = `${firstNumberValue} ${operationLabel} ${secondNumberValue} = ${resultat}`
 }
+
+function gestionDuneOperation() {
+    const firstNumberValue = 1 * document.getElementById("firstNumber").value;
+    const secondNumberValue = 1 * document.getElementById("secondNumber").value;
+    const operation = document.getElementById("operation").value;
+
+    let resultat = 0;
+    if (operation === "1") {
+        resultat = firstNumberValue + secondNumberValue;
+    }
+    if (operation === "2") {
+        resultat = firstNumberValue * secondNumberValue;
+    }
+    console.log("==========================================");
+    console.log(firstNumberValue, secondNumberValue, operation, typeof operation, resultat);
+    console.log("==========================================");
+    const operationLabel = operation === "1" ? "+" : "*";
+    document.getElementById("resultat").innerHTML = `${firstNumberValue} ${operationLabel} ${secondNumberValue} = ${resultat}`
+}
